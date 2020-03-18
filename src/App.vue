7274300986bbc5502a2924b5bc2c0d7823519d8f
miao qiu <qm1005465512@163.com>
2020-03-18 10:13:15 +0800

    路由信息

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,14 @@
 </template>
 
 <script>
+import Cookies from 'js-cookie'
+
 export default {
-  name: 'App'
+  name: 'App',
+  mounted() {
+    // Cookies.remove('Admin-Token')
+    //
+    // Cookies.set('Admin-Token', 'admin')
+  }
 }
 </script>
